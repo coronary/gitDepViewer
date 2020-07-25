a0dec0e128749d33044aae8e0fc2fa1e967af79f
Collin <cjdekalb@gmail.com>
2020-07-25 09:15:21 -0700

    turned results into a component

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="results">
+        <ul>
+            <h3 v-if='listings'>{{ title }}</h3>
+            <!--<h3 v-else>No Dependencies</h3>-->
+            <li v-for="(ver, name) in listings" v-bind:key=name><a v-bind:href=npm+name target="_blank">  {{ name }}: {{ ver}}</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Results',
+    data () {
+        return {
+            npm: 'https://www.npmjs.com/package/'
+        }
+    },
+    props: {
+        listings: Object,
+        title: String
+    },
+    methods: { },
+}
+
+</script>
+
+<style>
+
+
+</style>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,28 +1,23 @@
 <template>
     <div id="search">
         <input v-on:keyup.enter="search" v-model="searchTerm" type="text" placeholder="paste in git url">
-        <h2 v-if="packName">Dependencies for {{ packName }}</h2>
-        <ul>
-            <h3 v-if="deps">Dependencies</h3>
-            <h3 v-else>No Dependencies</h3>
-            <li v-for="(ver, name) in deps" v-bind:key=name><a v-bind:href=npm+name>  {{ name }}: {{ ver}}</a></li>
-            <h3 v-if="devdeps">Dev Dependencies</h3>
-            <h3 v-else>No Dev Dependencies</h3>
-            <li v-for="(ver, name) in devdeps" v-bind:key=name><a v-bind:href=npm+name>{{ name }}: {{ ver}}</a></li>
-        </ul>
+        <h2 v-if="isUrl(packName)">Dependencies for {{ packName }}</h2>
+        <results v-bind:listings=deps title='Dependencies'></results>
+        <results v-bind:listings=devdeps title='Dev Dependencies'></results>
     </div>
 </template>
 
 <script>
+import Results from './Results.vue'
 
 export default {
     name: 'Search',
+    components: {
+        Results
+    },
     data () {
         return {
             searchTerm: '',
-            rawUrl: 'https://raw.githubusercontent.com',
-            packUrlPartial: '/master/package.json',
-            npm: 'https://www.npmjs.com/package/',
             deps: null,
             devdeps: null
         }
@@ -44,9 +39,11 @@ export default {
     },
     computed: {
         packUrl: function () {
+            let rawUrl = 'https://raw.githubusercontent.com';
+            let packUrlPartial = '/master/package.json';
             if(this.isUrl(this.searchTerm)){
                 let splitted = this.searchTerm.split('github.com')
-                return this.rawUrl + splitted[splitted.length - 1] + this.packUrlPartial
+                return rawUrl + splitted[splitted.length - 1] + packUrlPartial
             }else {
                 return ''
             }
